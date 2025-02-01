ae6fc5bfef4d708a9b77ba3702d283ad04f895e6
amazing-rachel <rachel.mercado@ucalgary.ca>
2025-01-31 23:21:10 -0700

    part 2.5

---
+++ ex2.5.py
@@ -0,0 +1,52 @@
+# SAME AS 2.3 BUT WITH TIMING
+
+import json
+import timeit
+with open("large-file.json", "r", encoding = "UTF=8") as file:
+    content = json.load(file)
+
+
+def change_content(data):
+    for i in data:
+        if isinstance(i,dict):
+            if 'payload' in i:
+                if 'size' in i['payload']:
+                    i['payload']['size'] = 42
+                elif 'release' in i['payload']:
+                    if 'assets' in i['payload']['release']:
+                        for j in i['payload']['release']['assets']:
+                            j['size'] = 42
+                    elif 'repo' in i['payload']['release']:
+                        for j in i['payload']['release']['repo']:
+                            j['size'] = 42 
+                elif 'pull_request' in i['payload']:
+                    if 'head' in  i['payload']['pull_request']:
+                          if 'repo' in i['payload']['pull_request']['head']:
+                                repo = i['payload']['pull_request']['head']['repo']
+                                if isinstance(repo, dict):
+                                    repo['size'] = 42
+                                else:
+                                    i['payload']['pull_request']['head']['size'] = 42
+                    if 'base' in  i['payload']['pull_request']:
+                        if 'repo' in i['payload']['pull_request']['base']:
+                            i['payload']['pull_request']['base']['repo']['size'] = 42
+                elif 'forkee' in i['payload']:
+                    i['payload']['forkee']['size'] = 42
+                            
+                    
+                                    
+
+    return data
+              
+start = timeit.default_timer()
+for i in range(10):
+    changed_content = change_content(content)
+end = timeit.default_timer()
+print('The everage time across the ten repetitions is:', (end-start)/10, 'seconds.')
+
+changed_content = changed_content[::1]
+with open("output.2.3.json", "w") as file:
+    json.dump(changed_content, file, indent=4, separators=(',', ':'))
+
+
+
